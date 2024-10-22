317a38f6e8ad885b16f63b283ae958480e3d35dc
RoberGF <rgfrasta@gmail.com>
2024-10-22 19:32:31 +0200

    second commit adding callbacks loop

---
+++ bucle_callbacks.mjs
@@ -0,0 +1,36 @@
+'use strict';
+
+function escribeTras2Segundos(texto, instruccionsParaDespues) {
+    setTimeout(function () {
+        console.log(texto);
+        instruccionsParaDespues()
+    }, 2000)
+}
+
+/* esto es un bucle que repite 5 veces
+for (let n = 1; n < 5; n++){
+    escribeTras2Segundos('texto1', function () {
+        console.log('fin')
+    })
+}
+*/
+
+
+function bucleSerie(n, fn, callback) {
+    if (n == 0) {
+        //termino el bucle
+        callback()
+        return
+    }
+    n = n - 1
+    fn('texto' + n, function () {
+        //esto de aqui es llamar a la funcion a si misma , recursion
+        bucleSerie(n, fn, callback)
+    })
+
+}
+
+
+bucleSerie(5, escribeTras2Segundos, function () {
+    console.log('fin')
+})
---
+++ pruebas.mjs
@@ -0,0 +1,26 @@
+import { createServer } from 'node:http'
+
+function showOne(callback, index = 0, step = 1, limit = 5, PORT = 8000) {
+    setTimeout(function () {
+        console.clear()
+        if (index < limit) {
+            index = index + step
+            callback(index)
+            showOne(callback, index)
+        } else {
+            console.log(`After ${index} seconds`)
+            server.listen(PORT)
+            console.log(`Server running at http://localhost:${PORT}/`)
+        }
+    }, step * 1000)
+}
+
+const server = createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+
+    response.end('Hello world')
+})
+
+showOne(function (number) {
+    console.log(number)
+})
